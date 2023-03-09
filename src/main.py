3e1b84825209bcedecc8e0671ee45c57154d8f8a
Maxim <lmaokeklol@gmail.com>
2023-03-09 21:38:58 +0300

    fix some mistakes in finite difference scheme

--- src/main.py
+++ src/main.py
@@ -8,16 +8,13 @@ from plotting import plot_temperature
 # ВХОДНАЯ ТОЧКА ПРОГРАММЫ
 if __name__ == '__main__':
 
-    # Сетка в преобразованных координатах (по x пока не масштабируем для простоты)
-    x = np.linspace(0, W, N_X, endpoint=True)
-    y = np.linspace(0, 1, N_Y, endpoint=True)
-
     # Шаги на сетке
-    dx = W/(N_X - 1)  # x, как в новых, так и в старых координатах меняется от 0 до W
-    dy = 1/(N_Y - 1)  # y в НОВЫХ координатах меняется от 0 до 1
+    dx = 1.0/(N_X - 1)  # x в НОВЫХ координатах меняется от 0 до 1
+    dy = 1.0/(N_Y - 1)  # y в НОВЫХ координатах меняется от 0 до 1
 
     # Инициализируем положение границы фазового перехода в начальный момент времени
-    F = init_f_vector(x=x)
+    # в НОВЫХ координатах
+    F = init_f_vector(dx=dx, n_x=N_X)
 
     # Начальное распределение температуры в НОВЫХ координатах
     T = init_temperature()
@@ -33,24 +30,37 @@ if __name__ == '__main__':
 
     # Инициализируем переменные для температуры и положения свободной границы на новом шаге по времени
     T_new = np.copy(T)
+    T_old = np.copy(T)
     F_new = np.copy(F)
+    F_old = np.copy(F)
+
+    K = 2  # Число итераций на одном шаге
 
     while t_step < N_t:
-        print("### ВЫЧИСЛЯЮ ПОЛОЖЕНИЕ ГРАНИЦЫ, ШАГ = " + str(t_step) + " ###")
-        F_new = recalculate_boundary(F=F_new, T=T, dy=dy)
-        print("### ВЫЧИСЛЯЮ ТЕМПЕРАТУРУ, ШАГ = " + str(t_step) + " ###")
-        T_new = predict_correct(
-            T=T_new,
-            F=F_new,
-            dx=dx,
-            dy=dy
-        )
-        print("### ТЕМПЕРАТУРА НА НОВОМ ШАГЕ РАССЧИТАНА ###")
-        print("### СОХРАНЯЮ ГРАФИК ###")
+        # print("### ВЫЧИСЛЯЮ ПОЛОЖЕНИЕ ГРАНИЦЫ, ШАГ = " + str(t_step) + " ###")
+        # print("### ВЫЧИСЛЯЮ ТЕМПЕРАТУРУ, ШАГ = " + str(t_step) + " ###")
+
+        # Итерационный метод
+        for k in range(K):
+            T_new = predict_correct(
+                T=T_old,
+                F_new=F_new,
+                F_old=F_old,
+                dx=dx,
+                dy=dy
+            )
+            F_new = recalculate_boundary(F=F_old, T=T_new, dy=dy, dx=dx)
+
+        T_old = np.copy(T_new)
+        F_old = np.copy(F_new)
+
+        # print("### ТЕМПЕРАТУРА НА НОВОМ ШАГЕ РАССЧИТАНА ###")
+        # print("### СОХРАНЯЮ ГРАФИК ###")
         plot_temperature(
             T=reverse_transform(T_new, F_new),  # Преобразуем к исходным координатам
             time=round(t_step*(dt*t_0), 2),
             graph_id=t_step
         )
+
         t_step = t_step + 1
     print("### РАСЧЁТ ЗАВЕРШЁН ###")
--- src/one_phase/fd_scheme.py
+++ src/one_phase/fd_scheme.py
@@ -3,28 +3,31 @@ from linear_algebra.tdma import tdma
 import numpy as np
 
 
-def predict_correct(T, F, dx, dy):
+def predict_correct(T, F_new, F_old, dx, dy):
     """
     Расчёт температуры в новых координатах на новом шаге по времени с помощью схемы предиктор-корректор.
     T – значения температуры на сетке в НОВЫХ координатах
-    F_new – положение границы фазового перехода
-    F_old – положение границы фазового перехода на предыдущем шаге по времени
+    F_new – положение границы фазового перехода на n+1 шаге по времени
+    F_old – положение границы фазового перехода на n-ом шаге по времени
     dx – шаг по x на сетке в новых координатах
     dy – шаг по y на сетке в новых координатах
     """
+    inv_dx = 1.0/dx
+    inv_dt = 1.0/dt
+    inv_dy = 1.0/dy
 
     temp_T = np.copy(T)
     new_T = np.copy(T)
 
-    alpha_0 = 0  # Из левого граничного условия по x
-    beta_0 = T_ice / T_0  # Из левого граничного условия по x
+    alpha_0 = 0.0  # Из левого граничного условия по x (1-го рода)
+    beta_0 = T_ice/T_0  # Из левого граничного условия по x
 
-    a = c = np.ones((N_X - 1,)) * (-dt / (2 * dx ** 2))
-    b = np.ones((N_X - 1,)) * (1 + dt / (dx ** 2))
+    a = c = np.ones((N_X - 1,)) * 0.5 * inv_dx * inv_dx * -dt
+    b = np.ones((N_X - 1,)) * (1 + dt * inv_dx * inv_dx)
 
-    a_y = np.empty((N_Y - 1), )
-    b_y = np.empty((N_Y - 1), )
-    c_y = np.empty((N_Y - 1), )
+    a_y = np.empty((N_Y - 1),)
+    b_y = np.empty((N_Y - 1),)
+    c_y = np.empty((N_Y - 1),)
 
     # ПРЕДСКАЗАНИЕ ПО X
     for k in range(1, N_Y - 1):
@@ -32,86 +35,65 @@ def predict_correct(T, F, dx, dy):
         temp_T[k, :] = tdma(
             alpha_0=alpha_0,
             beta_0=beta_0,
-            u_r=T[k, N_X - 1],
+            condition_type=1,
+            phi=T_ice/T_0,
             a=a,
             b=b,
             c=c,
             f=T[k, :]
         )
 
+    alpha_0 = 0.0  # Из левого граничного условия по y (1-го рода)
+    beta_0 = T_ice/T_0  # Из левого граничного условия по y
+
     # ПРЕДСКАЗАНИЕ ПО Y
     for j in range(1, N_X - 1):
-        sigma_k = (1 / F[j]) ** 2
+        inv_F_new = 1.0/F_new[j]
         for k in range(0, N_Y - 1):
-            kappa_k = k*(3.0*temp_T[N_Y-1, j] - 4*temp_T[N_Y-2, j] + temp_T[N_Y-3, j])/(2*gamma*F[j]**2)
-            a_y[k] = -dt * (kappa_k / (4 * dy) + sigma_k / (2 * dy ** 2))
-            b_y[k] = 1 + dt * sigma_k / (dy ** 2)
-            c_y[k] = dt * (kappa_k / (4 * dy) - sigma_k / (2 * dy ** 2))
+            y = k*dy
+            kappa_k = y * inv_F_new * (inv_dt * (F_new[j] - F_old[j]) +
+                                       2 * (0.5 * inv_dx * (F_new[j + 1] - F_new[j - 1]))**2 -
+                                       inv_dx * inv_dx * (F_new[j + 1] - 2 * F_new[j] + F_new[j - 1]))
+            sigma_k = W * W * inv_F_new * inv_F_new + \
+                      (y * 0.5 * inv_dx * inv_F_new * (F_new[j + 1] - F_new[j - 1])) ** 2
+            a_y[k] = -dt * (0.25 * inv_dy * kappa_k + 0.5 * inv_dy * inv_dy * sigma_k)
+            b_y[k] = 1 + dt * inv_dy * inv_dy * sigma_k
+            c_y[k] = dt * (0.25 * inv_dy * kappa_k - 0.5 * inv_dy * inv_dy * sigma_k)
 
         temp_T[:, j] = tdma(
             alpha_0=alpha_0,
             beta_0=beta_0,
-            u_r=T[N_Y - 1, j],
+            condition_type=1,  # Граничное условие 1-го рода
+            phi=T_0/T_0,  # На границе ф.п. T = T_0)
             a=a_y,
             b=b_y,
             c=c_y,
-            f=T[:, j]
+            f=temp_T[:, j]
         )
 
     # КОРРЕКЦИЯ
     for j in range(1, N_X - 1):
-        sigma_k = (1 / F[j]) ** 2
+        inv_F_new = 1.0 / F_new[j]
         for k in range(1, N_Y - 1):
-            kappa_k = k*(3.0 * temp_T[N_Y - 1, j] - 4 * temp_T[N_Y - 2, j] + temp_T[N_Y - 3, j]) / (2 * gamma * F[j] ** 2)
-            m_1 = (temp_T[k, j + 1] - 2.0 * temp_T[k, j] + temp_T[k, j - 1]) / (dx ** 2)
-            m_2 = sigma_k * (temp_T[k + 1, j] - 2.0 * temp_T[k, j] + temp_T[k - 1, j]) / (dy ** 2)
-            m_3 = kappa_k * (temp_T[k + 1, j] - temp_T[k - 1, j]) / (2.0 * dy)
-            new_T[k, j] = T[k, j] + dt * (m_1 + m_2 + m_3)
+            y = k * dy
+            kappa_k = y * inv_F_new * (inv_dt * (F_new[j] - F_old[j]) +
+                                       2 * (0.5 * inv_dx * (F_new[j + 1] - F_new[j - 1])) ** 2 -
+                                       inv_dx * inv_dx * (F_new[j + 1] - 2 * F_new[j] + F_new[j - 1]))
 
-    return temp_T
+            sigma_k = W * W * inv_F_new * inv_F_new + \
+                      (y * 0.5 * inv_dx * inv_F_new * (F_new[j + 1] - F_new[j - 1])) ** 2
 
+            m_1 = inv_dx * inv_dx * (temp_T[k, j + 1] - 2.0 * temp_T[k, j] + temp_T[k, j - 1])
 
-def split_scheme(T, F, dx, dy):
-    temp_T = np.copy(T)
+            m_2 = sigma_k * inv_dy * inv_dy * (temp_T[k + 1, j] - 2.0 * temp_T[k, j] + temp_T[k - 1, j])
 
-    alpha_0 = 0  # Из левого граничного условия по x
-    beta_0 = T_ice / T_0  # Из левого граничного условия по x
+            m_3 = kappa_k * 0.5 * inv_dy * (temp_T[k + 1, j] - temp_T[k - 1, j])
 
-    a = c = np.ones((N_X - 1,)) * (-dt / (dx ** 2))
-    b = np.ones((N_X - 1,)) * (1 + 2 * dt / (dx ** 2))
+            m_4 = 0.25 * inv_dx * inv_dx * inv_dy * inv_F_new * y * (F_new[j + 1] - F_new[j - 1]) * (temp_T[k+1, j+1] -
+                                                                                                     temp_T[k+1, j-1] -
+                                                                                                     temp_T[k-1, j+1] +
+                                                                                                     temp_T[k-1, j-1])
 
-    a_y = np.empty((N_Y - 1), )
-    b_y = np.empty((N_Y - 1), )
-    c_y = np.empty((N_Y - 1), )
-
-    for k in range(1, N_Y - 1):
-        # ПРОГОНКА
-        temp_T[k, :] = tdma(
-            alpha_0=alpha_0,
-            beta_0=beta_0,
-            u_r=T[k, N_X - 1],
-            a=a,
-            b=b,
-            c=c,
-            f=T[k, :]
-        )
-
-    for j in range(1, N_X - 1):
-        sigma_k = (1 / F[j]) ** 2
-        for k in range(0, N_Y - 1):
-            kappa_k = k*(3.0*temp_T[N_Y-1, j] - 4*temp_T[N_Y-2, j] + temp_T[N_Y-3, j])/(2*gamma*F[j]**2)
-            a_y[k] = -dt * (kappa_k / (2 * dy) + sigma_k / (dy ** 2))
-            b_y[k] = 1 + 2 * dt * sigma_k / (dy ** 2)
-            c_y[k] = dt * (kappa_k / (2 * dy) - sigma_k / (dy ** 2))
-
-        temp_T[:, j] = tdma(
-            alpha_0=alpha_0,
-            beta_0=beta_0,
-            u_r=T[N_Y - 1, j],
-            a=a_y,
-            b=b_y,
-            c=c_y,
-            f=T[:, j]
-        )
+            new_T[k, j] = T[k, j] + dt * (m_1 + m_2 + m_3 - m_4)
 
     return temp_T
