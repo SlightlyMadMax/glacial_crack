0d94031c5e65a1be92e56964eeb79306904968f3
Maxim <lmaokeklol@gmail.com>
2023-04-16 15:00:09 +0300

    prepare structure for two phase problem

---
+++ src/one_phase/nonuniform_y_grid/grid_generation.py
@@ -0,0 +1,11 @@
+from parameters import *
+import math
+
+
+def get_node_coord(t: float):
+    """
+    Функция для генерации неравномерной сетки. Сгущает узлы ближе к единице.
+    :param t: координата на равномерной сетке на интервале (0, 1).
+    :return: координата на неравномерной сетке.
+    """
+    return 1.0 - math.exp(-s * t) + math.exp(-s)
