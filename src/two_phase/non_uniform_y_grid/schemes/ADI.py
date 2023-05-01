66213da9f73c79d6f502002610df1d83e0c65c4a
Maxim <lmaokeklol@gmail.com>
2023-05-02 02:17:35 +0300

    more refactoring

--- src/two_phase/non_uniform_y_grid/schemes/ADI.py
+++ src/two_phase/non_uniform_y_grid/schemes/ADI.py
@@ -5,10 +5,14 @@ import numpy as np
 
 
 def find_rhs(T, F_new, F_old, j: int, i: int):
-
-    j_int = int(0.5*(N_Y - 1))
+    inv_F_new = 1.0 / F_new[i]
+    inv_FH_new = 1.0 / (H - F_new[i])
+    inv_dx = 1.0 / dx
+    inv_dt = 1.0 / dt
+    j_int = int(0.5 * (N_Y - 1))
     chi = 1.0 if j < j_int else c_ice*rho_ice*k_w/(c_w*rho_w*k_ice)
 
+    # Координата y
     if j == 0:
         y = 0.0
     elif j == N_Y - 1:
@@ -16,13 +20,11 @@ def find_rhs(T, F_new, F_old, j: int, i: int):
     else:
         y = get_node_coord(2 * j / (N_Y - 1))
 
-    inv_F_new = 1.0 / F_new[i]
-    inv_FH_new = 1.0/(H - F_new[i])
-    inv_dx = 1.0/dx
-    inv_dt = 1.0/dt
-
     # Производная F по x
     df_dx = F_new[i + 1] - F_new[i - 1]
+
+    # Вторая производная T по x
+    d_xx = T[j, i + 1] - 2.0 * T[j, i] + T[j, i - 1]
 
     # Первая и смешанная производная T по y
     if j == 0:
@@ -52,18 +54,15 @@ def find_rhs(T, F_new, F_old, j: int, i: int):
         # TODO: second order for d_xy
         d_xy = (T[j + 1, i + 1] - T[j + 1, i - 1] - T[j - 1, i + 1] + T[j - 1, i - 1]) / (dx * (h_0 + h_2))
 
-    # Вторая производная T по x
-    d_xx = T[j, i + 1] - 2.0 * T[j, i] + T[j, i - 1]
-
     if j <= j_int:
         kappa = y * inv_F_new * (inv_dt * (F_new[i] - F_old[i]) + 0.5 * inv_F_new * inv_dx * inv_dx * df_dx * df_dx -
                                 inv_dx * inv_dx * (F_new[i + 1] - 2 * F_new[i] + F_new[i - 1]))
-        zeta = -2 * y * inv_F_new * df_dx
+        zeta = - y * inv_F_new * df_dx * inv_dx
     else:
         kappa = inv_FH_new * (inv_dt * (2.0 - y) * (F_new[i] - F_old[i]) +
                               0.5 * inv_FH_new * (y - 1.0) * inv_dx * inv_dx * df_dx * df_dx -
                               (y - 1.0) * inv_dx * inv_dx * (F_new[i + 1] - 2 * F_new[i] + F_new[i - 1]))
-        zeta = -2 * (y - 1.0) * inv_FH_new * df_dx
+        zeta = - (y - 1.0) * inv_FH_new * df_dx * inv_dx
 
     return T[j, i] + \
         dt * (d_xx * inv_dx * inv_dx +
@@ -83,13 +82,11 @@ def solve(T, F_new, F_old):
     b_y = np.empty((N_Y - 1), )
     c_y = np.empty((N_Y - 1), )
 
-    alpha_0 = 0.0  # Из левого граничного условия по y (1-го рода)
-    beta_0 = T_ice/T_0  # Из левого граничного условия по y
-
     rhs = np.empty(N_Y,)
     for i in range(1, N_X - 1):
         inv_F_new = 1.0 / F_new[i]
         inv_FH_new = 1.0 / (H - F_new[i])
+        df_dx = F_new[i + 1] - F_new[i - 1]
         for j in range(0, N_Y - 1):
             chi = 1.0 if j < j_int else c_ice * rho_ice * k_w / (c_w * rho_w * k_ice)
             if j == 0:
@@ -101,13 +98,12 @@ def solve(T, F_new, F_old):
                 h = get_node_coord(2 * (j + 1) / (N_Y - 1)) - y
                 h_0 = y - get_node_coord(2 * (j - 1) / (N_Y - 1))
 
-            df_dx = F_new[i + 1] - F_new[i - 1]
             if j <= j_int:
                 sigma_j = W * W * inv_F_new * inv_F_new + \
-                          (0.5 * y * inv_dx * inv_F_new * df_dx)*(0.5 * y * inv_dx * inv_F_new * df_dx)
+                          (0.25 * y * inv_dx * inv_F_new * df_dx * y * inv_dx * inv_F_new * df_dx)
             else:
                 sigma_j = W * W * inv_FH_new * inv_FH_new + \
-                          (0.5 * (1.0 - y) * inv_dx * inv_FH_new * df_dx) * (0.5 * (1.0 - y) * inv_dx * inv_FH_new * df_dx)
+                          (0.25 * (1.0 - y) * (1.0 - y) * inv_dx * inv_FH_new * df_dx * inv_dx * inv_FH_new * df_dx)
 
             a_y[j] = -2.0 * dt * sigma_j / (chi * (h * (h + h_0)))
             c_y[j] = -2.0 * dt * sigma_j / (chi * (h_0 * (h + h_0)))
@@ -119,19 +115,16 @@ def solve(T, F_new, F_old):
 
         # ПРОГОНКА
         temp_T[:, i] = tdma(
-            alpha_0=alpha_0,
-            beta_0=beta_0,
+            alpha_0=0.0,  # Из левого граничного условия по y (1-го рода)
+            beta_0=T_ice / T_0,  # Из левого граничного условия по y
             condition_type=1,
-            phi=T_w/T_0,
+            phi=T_w / T_0,
             a=a_y,
             b=b_y,
             c=c_y,
             f=rhs
         )
-        temp_T[j_int, i] = T_0/T_0
-
-    alpha_0 = 1.0  # Из левого граничного условия по x (2-го рода)
-    beta_0 = 0.0  # Из левого граничного условия по x
+        temp_T[j_int, i] = T_0 / T_0
 
     rhs = np.empty(N_X,)
     for j in range(1, N_Y - 1):
@@ -149,13 +142,14 @@ def solve(T, F_new, F_old):
             rhs[i] = temp_T[j, i] - dt * inv_dx * inv_dx * d_xx / chi
 
         new_T[j, :] = tdma(
-            alpha_0=alpha_0,
-            beta_0=beta_0,
-            condition_type=2,  # Граничное условие 2-го рода
+            alpha_0=1.0,  # Из левого граничного условия по x (2-го рода)
+            beta_0=0.0,  # Из левого граничного условия по x
+            condition_type=2,
             phi=0.0,
             a=a,
             b=b,
             c=c,
             f=rhs
         )
+
     return new_T
