595cd614e809ca2307f45f1522cf486824d50960
Maxim <lmaokeklol@gmail.com>
2023-05-02 00:34:44 +0300

    refactor plotting files

--- src/one_phase/uniform_grid/plotting.py
+++ src/one_phase/uniform_grid/plotting.py
@@ -5,23 +5,17 @@ import numpy as np
 
 def plot_non_transformed(T, F, time: float, graph_id: int):
     x = np.linspace(0, 1.0, N_X)
-    y = np.linspace(0, 2.0, N_Y)
-    j_int = int(0.5 * (N_Y - 1))
+    y = np.linspace(0, 1.0, N_Y)
 
     X, Y = np.meshgrid(x, y)
 
     X = X * W
-    for i in range(N_X):
-        for j in range(N_Y):
-            if j <= j_int:
-                Y[j, i] = Y[j, i] * F[i]
-            else:
-                Y[j, i] = (Y[j, i] - 1.0) * (H - F[i]) + F[i]
+    Y[:, :] = Y[:, :] * F[:]
 
     fig = plt.figure()
     ax = plt.axes()
-    # plt.plot(X, Y, marker=".", markersize=0.5, color='red', linestyle='none')  # сетка
-    plt.contourf(X, Y, T_0 * T - T_0, 50, cmap="viridis")
+    # plt.plot(X, Y[j_int, :], marker=".", markersize=0.1, color='red', linestyle='none')  # сетка
+    plt.contourf(X, Y, T_0 * T - T_0, 100, cmap="viridis")
     plt.colorbar()
 
     title = f"time = {str(time)} h\n dx = 1/{str(N_X)} m, dy = 1/{str(N_Y)} m, dt = {str(round(dt * t_0 / 3600.0, 2))} h"
@@ -31,3 +25,4 @@ def plot_non_transformed(T, F, time: float, graph_id: int):
     ax.set_ylabel("y, m")
     plt.savefig(f"graphs/temperature/T_{str(graph_id)}.png")
     plt.show()
+    # plt.close()
