8678cb11c2723a78b832cc3fd8638389a613d3ae
Maxim <lmaokeklol@gmail.com>
2023-05-05 12:42:28 +0300

    two phase grid generation update

--- src/two_phase/non_uniform_y_grid/grid_generation.py
+++ src/two_phase/non_uniform_y_grid/grid_generation.py
@@ -1,11 +1,23 @@
 import math
-from parameters import s
+from parameters import s, N_Y
 
 
-def get_node_coord(t: float):
+def get_node_coord(j: int, j_int: int) -> float:
     """
     Функция для генерации неравномерной сетки. Сгущает узлы ближе к фазовой границе.
-    :param t: координата на равномерной сетке на интервале (0, 2).
+    :param t: координата на равномерной сетке на интервале (0, 1).
     :return: координата на неравномерной сетке.
     """
-    return 1.0 - math.log(2.0 / t - 1.0) / s
+    t = 2 * j / (N_Y - 2)
+
+    if j == 0:
+        return 0.0
+    elif j == j_int:
+        return 1.0
+    elif j == N_Y - 1:
+        return 2.0
+    elif j < j_int:
+        return 1.0 - math.exp(-s * t) + math.exp(-s)
+    else:
+        t = (2 * N_Y - 4 - 2 * j) / (N_Y - 2)
+        return 1.0 + math.exp(-s * t) - math.exp(-s)
